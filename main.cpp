313063b2e5a9e8025be6a57ea15bbddda6055f65
sunbo.kie <sunbo.kie@bytedance.com>
2021-06-13 20:02:07 +0800

    remove debug code

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ std::string exec(const std::string s){
     auto file = popen(s.c_str(),"r");
     if(file==nullptr){
         std::cout<<"popen failed"<<std::endl;
+        return "";
     }
     std::stringstream ss;
     
@@ -29,7 +30,6 @@ bool is_number(const std::string& s)
 
 int get_current_temp(){
     auto temp_str = exec("vcgencmd measure_temp");
-    temp_str = "temp=49.0'C";
     std::regex pattern("temp=(.+)'C");
     std::match_results<std::string::iterator> match_result;
     std::regex_match(temp_str.begin(),temp_str.end(),match_result,pattern);
@@ -37,7 +37,6 @@ int get_current_temp(){
         return -1;
     }
     auto temp_num_str = match_result[1].str();
-    std::cout<<temp_num_str<<std::endl;
     if(is_number(temp_num_str)){
         return std::round(std::stod(temp_num_str));
     }
